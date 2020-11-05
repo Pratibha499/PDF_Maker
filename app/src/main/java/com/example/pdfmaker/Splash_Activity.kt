a2f69959c581990223489662e7be7138ef0cd279
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-06 08:02:53 +1400

    Splash Activity is created by  adding Theme to the background

--- app/src/main/java/com/example/pdfmaker/Splash_Activity.kt
+++ app/src/main/java/com/example/pdfmaker/Splash_Activity.kt
@@ -1,5 +1,6 @@
 package com.example.pdfmaker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,7 @@ class Splash_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish();
     }
 }
