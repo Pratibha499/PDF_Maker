c98329c17840043a64a3c9399f5b015f1c53c275
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-11-13 23:30:27 +0530

    create method onOptionsItemSelected

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.pdfmaker
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.ContentFrameLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -55,5 +57,14 @@ class MainActivity : AppCompatActivity() {
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id =item.itemId
+        if(id==android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
