243f189fd0aa5c2405bd6383354ec3804ad7e480
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-11-13 23:08:04 +0530

    add coordinateLayout variable

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     // lateinit is used as promise that the variable will be initialised later
     private lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
+    lateinit var coordinateLayout: CoordinatorLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
