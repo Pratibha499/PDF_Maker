5cef221d0caa31eaa5134268db8c2c6cd146a937
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-14 00:32:58 +1400

    Toolbar setUp done

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.example.pdfmaker
 
 import android.os.Bundle
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
-    // lateinit is used as promise that the variable  will be initialised later
-    lateinit var toolbar: Toolbar
+    // lateinit is used as promise that the variable will be initialised later
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // id of Views
+        // id of Views are initialised to the variables
         toolbar = findViewById(R.id.toolbar)
 
+        // function call
+        setUpToolbar()
     }
 
-    // Toolbar Setup
+    /*
+              Toolbar Setup
+    */
     private fun setUpToolbar() {
         setSupportActionBar(toolbar)
         // Title set on toolbar
