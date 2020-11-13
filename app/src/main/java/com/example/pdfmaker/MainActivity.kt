98e84724d36ab24f0774a50b50fabf81918908fb
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-14 00:08:22 +1400

    setSupportActionBar() method used to set toolbar as the app bar for the activity

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.pdfmaker
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit is used as promise that the variable  will be initialised later
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // id of Views
+        toolbar = findViewById(R.id.toolbar)
+
     }
 
     // Toolbar Setup
     private fun setUpToolbar() {
-
+        setSupportActionBar(toolbar)
     }
 }
