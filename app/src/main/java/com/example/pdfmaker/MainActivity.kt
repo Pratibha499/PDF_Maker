93a0814de8fa171b0cdfc0e9acc5c3b57cfc767b
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-14 00:45:48 +1400

    String "Open Navigation Drawer" is declared in strings.xml which opens the Navigation Drawer

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.pdfmaker
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
     // lateinit is used as promise that the variable will be initialised later
     private lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         // id of Views are initialised to the variables
         toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
 
         // function call
         setUpToolbar()
+
+        val ActionBarDrawerToggle: ActionBarDrawerToggle(this, drawerLayout, )
     }
 
     /*
-              Toolbar Setup
+           Toolbar Setup
     */
     private fun setUpToolbar() {
         setSupportActionBar(toolbar)
