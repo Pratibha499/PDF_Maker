ae3e6e4900146a852547d526d57b6d8b05a63c67
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-22 08:54:40 +1400

    Function for API-19

--- app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
+++ app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
@@ -1,10 +1,17 @@
 package com.example.pdfmaker.helper
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
 
 object RealPathUtil {
 
     /** Annotation by Android Lint Tool **/
     @SuppressLint("NewApi")
 
+    fun getRealPathFromURI_API19(context: Context, uri: Uri): String? {
+
+
+        return ""
+    }
 }
