8a474f466c86df7e3f28e90bb0bf6b8b2d14ced1
kapil kumar <kapil.kumar_cs18@gla.ac.in>
2020-11-21 22:45:19 +0530

    add xml and .kt file for createpdf

---
+++ app/src/main/java/com/example/pdfmaker/CreatePdf.kt
@@ -0,0 +1,11 @@
+package com.example.pdfmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePdf : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_pdf)
+    }
+}
