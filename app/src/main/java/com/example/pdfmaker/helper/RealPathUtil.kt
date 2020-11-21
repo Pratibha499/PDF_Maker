6e82389e9c9b48b3ec0ef789bd327c70f8aef6f9
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-22 08:46:27 +1400

    "New Api" suppresses all warnings that would tell if you're using any API introduced after your minSdkVersion

--- app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
+++ app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
@@ -1,5 +1,10 @@
 package com.example.pdfmaker.helper
 
+import android.annotation.SuppressLint
+
 object RealPathUtil {
-    
+
+    /** Annotation by Android Lint Tool **/
+    @SuppressLint("NewApi")
+
 }
