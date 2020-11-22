21b141104b95c6aa6a9fea51a9d395793746d27d
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-22 22:27:51 +1400

    contentResolver used to access data in content provider

--- app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
+++ app/src/main/java/com/example/pdfmaker/helper/RealPathUtil.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.provider.DocumentsContract
+import android.provider.MediaStore
 
 object RealPathUtil {
 
@@ -19,6 +20,15 @@ object RealPathUtil {
             val idArraySplit = documentId.split(":").toTypedArray()
             if (idArraySplit.size == 2) {
                 val id = idArraySplit[1]
+                val column = arrayOf(MediaStore.Images.Media.DATA)
+                // where id is equal to
+                val sel = MediaStore.Images.Media._ID + "=?"
+                // Media provider to access all Image type files on the External Storag
+                // contentResolver used to access data in content provider
+                val cursor = context.contentResolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,     // content URI
+                    column, sel, arrayOf(id), null
+                )
             }
         }
 
