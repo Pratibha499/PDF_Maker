331cbb97958f4b1ec9326dfa2c602fc9e3024549
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-22 23:39:21 +1400

    ActivityCompat is a helper for accessing features in activity.

--- app/src/main/java/com/example/pdfmaker/CreatePdf.kt
+++ app/src/main/java/com/example/pdfmaker/CreatePdf.kt
@@ -1,7 +1,10 @@
 package com.example.pdfmaker
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class CreatePdf : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +12,12 @@ class CreatePdf : AppCompatActivity() {
         setContentView(R.layout.activity_create_pdf)
 
         /** Request Permission to Read and Write to External Storage **/
-
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,         // Request permission to read
+                Manifest.permission.WRITE_EXTERNAL_STORAGE         // Request permission to write
+            ), PackageManager.PERMISSION_GRANTED
+        )
     }
 }
