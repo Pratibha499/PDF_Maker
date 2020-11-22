a28ba075d292630cb9bdae1f9374b1613ea29aa2
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-22 23:32:15 +1400

    Request user to grant permission to access External Storage in android device

--- app/src/main/java/com/example/pdfmaker/CreatePdf.kt
+++ app/src/main/java/com/example/pdfmaker/CreatePdf.kt
@@ -1,11 +1,14 @@
 package com.example.pdfmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class CreatePdf : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_pdf)
+
+        /** Request Permission to Read and Write to External Storage **/
+
     }
 }
