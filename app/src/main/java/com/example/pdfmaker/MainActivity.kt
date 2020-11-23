da0157f03b64257c8aa96562f29ddb8ed8e2f36f
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-11-24 12:11:55 +1400

    Camera button added in activity_create_pdf.xml file

--- app/src/main/java/com/example/pdfmaker/MainActivity.kt
+++ app/src/main/java/com/example/pdfmaker/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
